36aef30c9f28003b00ddccf0ac444fa4af82d3e1
0nida <piekute.rimante@gmail.com>
2017-02-04 19:28:13 +0200

    start

---
+++ members/members.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $.get("members.json", function (data) {
+        addAllLines(data);
+    });
+});
+
+function addAllLines(members) {
+    for (var i = 0; i < members.length; i++) {
+        addLine(members[i]);
+    }
+}
+
+
+function addLine(member) {
+    $("tbody").append("<tr><td>" + member.name + "</td><td>" + member.date + "</td><td>" + member.email + "</td></tr>");
+}
